2248ac5d24d8ce39c7c2588c8d0734a9525937cb
RyanDMcAnulty <rdmcanulty1s@semo.edu>
2020-07-01 18:04:08 -0500

    Finished part 1.

---
+++ hw 02/hw02-1.R
@@ -0,0 +1,82 @@
+# Ryan McAnulty
+# HW02 Part 1: Intro to Basics
+# Calculate 30 + 12
+30 + 12
+# The answer should be 42
+
+# Section 1.2
+
+# Division
+121/1.1
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
+
+# Exponentiation without parentheses
+49^1/2
+
+# Exponentiation with parentheses
+49^(1/2)
+
+# "1.3 Variable assignment"
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# Assign the value 24 the variable d_merriami
+d_merriami <- 24
+
+# Assign the value 9 to d_ordii
+d_ordii <- 9
+
+# Check the results
+d_merriami 
+d_ordii
+
+### "1.4 Working with Variables"
+
+# Correcting the mistake
+d_merriami <- d_merriami + 5
+
+d_merriami
+
+# Add together the number of individuals of each species.
+# Store the result in 'total_k_rats'.
+total_k_rats <- d_merriami + d_ordii
+
+# Assign 1000 to a variable called 'area_sampled'
+area_sampled <- 1000
+
+# Divide 'total_k_rats' by 'area_sampled' to calculate 'density'.
+density <- total_k_rats/area_sampled
+
+# Show the value stored in density
+density
+
+# "1.5 Basic data types in R"
+# Assign 42 to numeric_var
+numeric_var <- 42
+
+# Assign "Dipodomys merriami" to species_name
+species_name <- "Dipodomys merriami"
+
+# Assign TRUE to is_mammal Remember: Case matters
+is_mammal <- TRUE
+
+# "1.6 What is the data type?"
+# Check class of numeric_var
+class(numeric_var)
+
+# Check class of species_name
+class(species_name)
+
+# Check class of is_mammal
+class(is_mammal)
+
+##
